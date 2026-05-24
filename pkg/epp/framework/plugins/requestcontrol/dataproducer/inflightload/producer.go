a8e738b28d2122b0ca5f83599526be269e45b395
Kaushik Mitra <kaushikmitra@google.com>
2026-05-24 16:35:01 +0000

    refactor(tokenload): rename CurrentRequestLoadDataKey to CurrentRequestEndpointImpactDataKey
    This rename better reflects that the attribute represents the speculative impact of the current request on endpoint load, rather than a global load value.

--- pkg/epp/framework/plugins/requestcontrol/dataproducer/inflightload/producer.go
+++ pkg/epp/framework/plugins/requestcontrol/dataproducer/inflightload/producer.go
@@ -71,7 +71,7 @@ func InFlightLoadProducerFactory(name string, rawParameters json.RawMessage, han
 		tokenEstimator:           NewSimpleTokenEstimator(),
 		addEstimatedOutputTokens: cfg.AddEstimatedOutputTokens,
 		dk:                       attrconcurrency.InFlightLoadDataKey.WithNonEmptyProducerName(name),
-		currentRequestLoadDK:     attrconcurrency.CurrentRequestLoadDataKey.WithNonEmptyProducerName(name),
+		currentRequestEndpointImpactDK: attrconcurrency.CurrentRequestEndpointImpactDataKey.WithNonEmptyProducerName(name),
 		PluginState:              fwkplugin.NewPluginState(ctx),
 	}, nil
 }
@@ -92,7 +92,7 @@ type InFlightLoadProducer struct {
 	addEstimatedOutputTokens bool
 	PluginState              *fwkplugin.PluginState
 	dk                       fwkplugin.DataKey
-	currentRequestLoadDK     fwkplugin.DataKey
+	currentRequestEndpointImpactDK fwkplugin.DataKey
 }
 
 // addedTokensEntry tracks a request's contribution to the global token and
@@ -209,7 +209,7 @@ func (p *InFlightLoadProducer) Produce(_ context.Context, request *fwksched.Infe
 		if p.addEstimatedOutputTokens {
 			tokens += p.tokenEstimator.EstimateOutput(inputTokens)
 		}
-		e.Put(p.currentRequestLoadDK.String(), &attrconcurrency.InFlightLoad{
+		e.Put(p.currentRequestEndpointImpactDK.String(), &attrconcurrency.InFlightLoad{
 			Tokens:   tokens,
 			Requests: 1,
 		})
@@ -451,7 +451,7 @@ func nonNeg(v int64) int64 {
 func (p *InFlightLoadProducer) Produces() map[fwkplugin.DataKey]any {
 	return map[fwkplugin.DataKey]any{
 		p.dk:                 attrconcurrency.InFlightLoad{},
-		p.currentRequestLoadDK: attrconcurrency.InFlightLoad{},
+		p.currentRequestEndpointImpactDK: attrconcurrency.InFlightLoad{},
 	}
 }
 
